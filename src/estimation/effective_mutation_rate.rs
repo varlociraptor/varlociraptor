c3be1ba824716c293922eda1c6bc5518519cfb38
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-08-04 14:45:49 +0200

    Estimate effective mutation rate and corresponding test case.

--- src/estimation/effective_mutation_rate.rs
+++ src/estimation/effective_mutation_rate.rs
@@ -7,6 +7,7 @@ use ordered_float::NotNaN;
 use itertools::Itertools;
 
 
+#[derive(Debug)]
 pub struct Estimate {
     pub effective_mutation_rate: f64,
     pub observations: BTreeMap<NotNaN<f64>, u64>
@@ -19,14 +20,18 @@ pub struct Estimate {
 /// # Arguments
 ///
 /// * `allele_frequencies` - allele frequencies of somatic passenger mutations (e.g. SNPs)
-pub fn estimate<F: IntoIterator<Item=NotNaN<f64>>>(allele_frequencies: F) -> Estimate {
+pub fn estimate<F: IntoIterator<Item=f64>>(allele_frequencies: F) -> Estimate {
     let mut observations = BTreeMap::new();
     for f in allele_frequencies {
         // count occurrences of 1 / f
-        *observations.entry(NotNaN::new(1.0 / *f).unwrap()).or_insert(0) += 1;
+        *observations.entry(NotNaN::new(1.0 / f).unwrap()).or_insert(0) += 1;
     }
     let freqs = observations.keys().map(|f| **f).collect_vec();
-    let counts = observations.values().map(|c| *c as f64).collect_vec();
+    // calculate the cumulative sum
+    let counts = observations.values().scan(0.0, |s, c| {
+        *s += *c as f64;
+        Some(*s)
+    }).collect_vec();
 
     let freqs = Matrix::new(freqs.len(), 1, freqs);
     let counts = Vector::new(counts);
@@ -34,10 +39,25 @@ pub fn estimate<F: IntoIterator<Item=NotNaN<f64>>>(allele_frequencies: F) -> Est
     let mut lin_mod = LinRegressor::default();
     lin_mod.train(&freqs, &counts);
 
-    let slope = lin_mod.parameters().unwrap()[0];
+    let slope = lin_mod.parameters().unwrap()[1];
 
     Estimate {
         effective_mutation_rate: slope,
         observations: observations
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use itertools::linspace;
+
+    #[test]
+    fn test_estimate() {
+        // example from Williams et al. Nature Genetics 2016.
+        let freqs = linspace(0.12, 0.25, 2539);
+        let estimate = estimate(freqs);
+        assert_relative_eq!(estimate.effective_mutation_rate, 596.16, epsilon=0.01);
     }
 }
