309b526ec0c40bf7ff445ab9e65145a143ff5771
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-10-31 08:01:39 +0100

    Cleaner and more flexible collection of variants.

--- src/utils.rs
+++ src/utils.rs
@@ -15,50 +15,77 @@ pub fn collect_variants(
     omit_indels: bool,
     indel_len_range: Option<Range<u32>>
 ) -> Result<Vec<Option<model::Variant>>, Box<Error>> {
+    let pos = record.pos();
+    let svlen = match record.info(b"SVLEN").integer() {
+        Ok(Some(svlen)) => Some(svlen[0].abs() as u32),
+        _ => None
+    };
+    let end = match record.info(b"END").integer() {
+        Ok(Some(end)) => Some(end[0] as u32),
+        _ => None
+    };
+    let inslen = match record.info(b"INSLEN").integer() {
+        Ok(Some(inslen)) => Some(inslen[0] as u32),
+        _ => None
+    };
     // TODO avoid cloning svtype
-    let svtypes = record.info(b"SVTYPE").string().map(|values| {
-        values.map(|values| values.into_iter().map(|svtype| svtype.to_owned()).collect_vec())
-    });
+    let svtype = match record.info(b"SVTYPE").string() {
+        Ok(Some(svtype)) => Some(svtype[0].to_owned()),
+        _ => None
+    };
 
-    let variants = if let Ok(Some(svtypes)) = svtypes {
-        // obtain svlen if it is present or raise error
-        let svlens = record.info(b"SVLEN").integer().map(|values| values.map(|values| values.to_owned()));
-        let pos = record.pos() as i32;
-        let svlens = match svlens {
-            Ok(Some(svlens)) => svlens,
-            _ => {
-                match record.info(b"END").integer() {
-                    Ok(Some(ends)) => ends.into_iter().map(|end| end - 1 - pos).collect_vec(),
-                    Err(e) => return Err(Box::new(e)),
-                    Ok(None) => return Err(Box::new(BCFError::MissingTag("SVLEN".to_owned())))
-                }
+    // check if len is within the given range
+    let is_valid_len = |svlen| {
+        if let Some(ref len_range) = indel_len_range {
+        // TODO replace with Range::contains once stabilized
+            if svlen < len_range.start && svlen >= len_range.end {
+                return false;
             }
-        };
-        svtypes.iter().zip(svlens).map(|(svtype, svlen)| {
-            let svlen = svlen.abs() as u32;
-            if let Some(ref len_range) = indel_len_range {
-                // TODO replace with Range::contains once stabilized
-                if svlen < len_range.start && svlen >= len_range.end {
-                    return None;
-                }
-            }
+        }
+        true
+    };
 
+    let variants = if let Some(svtype) = svtype {
+        vec![
             if omit_indels {
                 None
             } else if svtype == b"INS" {
-                Some(model::Variant::Insertion(svlen))
+                let svlen = match (svlen, inslen) {
+                    (Some(svlen), _)     => svlen,
+                    (None, Some(inslen)) => inslen,
+                    _ => {
+                        return Err(Box::new(BCFError::MissingTag("SVLEN or INSLEN".to_owned())));
+                    }
+                };
+                if is_valid_len(svlen) {
+                    Some(model::Variant::Insertion(svlen))
+                } else {
+                    None
+                }
             } else if svtype == b"DEL" {
-                Some(model::Variant::Deletion(svlen))
+                let svlen = match(svlen, end) {
+                    (Some(svlen), _)  => svlen,
+                    (None, Some(end)) => end - 1 - pos,
+                    _ => {
+                        return Err(Box::new(BCFError::MissingTag("SVLEN or END".to_owned())));
+                    }
+                };
+                if is_valid_len(svlen) {
+                    Some(model::Variant::Deletion(svlen))
+                } else {
+                    None
+                }
             } else {
                 None
             }
-        }).collect_vec()
+        ]
     } else {
         let alleles = record.alleles();
         let ref_allele = alleles[0];
 
         alleles.iter().skip(1).map(|alt_allele| {
             if alt_allele.len() == 1 && ref_allele.len() == 1 {
+                // SNV
                 if omit_snvs {
                     None
                 } else {
@@ -69,14 +96,10 @@ pub fn collect_variants(
                 None
             } else {
                 let indel_len = (alt_allele.len() as i32 - ref_allele.len() as i32).abs() as u32;
-                if let Some(ref len_range) = indel_len_range {
-                    // TODO replace with Range::contains once stabilized
-                    if indel_len < len_range.start && indel_len >= len_range.end {
-                        return None;
-                    }
-                }
 
                 if omit_indels {
+                    None
+                } else if is_valid_len(indel_len) {
                     None
                 } else if alt_allele.len() < ref_allele.len() {
                     Some(model::Variant::Deletion((ref_allele.len() - alt_allele.len()) as u32))
