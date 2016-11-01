5b9b41371d011ea08e0f4c94f582a24221aa8383
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-11-01 18:10:09 +0100

    Change alpha values.

--- src/estimation/fdr_bh.rs
+++ src/estimation/fdr_bh.rs
@@ -12,7 +12,7 @@ use Event;
 use model;
 
 
-const ALPHAS: [f64; 29] = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
+const ALPHAS: [f64; 28] = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0];
 
 
 fn collect_dist<E: Event>(calls: &bcf::Reader, event: &E, vartype: &model::VariantType) -> Result<Vec<NotNaN<f64>>, Box<Error>> {
@@ -101,7 +101,7 @@ pub fn control_fdr<E: Event, W: io::Write>(
         let mut record = Record { alpha: alpha, gamma: PHREDProb::from(Prob(1.0)) };
         let alpha = alpha.ln();
         for (&mkp, &event_prob) in mk_pvals.iter().zip(prob_dist.iter()) {
-            // the pvalues will be monotolically decreasing
+            // the pvalues will be monotonically decreasing
             if mkp <= alpha {
                 // the highest pval that is below the threshold
                 // record corresponding event probability
