a824e283012885a5779e511a4e49bf827aebd2f1
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-05-01 15:49:14 +0200

    Debugging.

--- src/model/priors/flat.rs
+++ src/model/priors/flat.rs
@@ -220,6 +220,7 @@ impl PairModel<ContinuousAlleleFreqs, DiscreteAlleleFreqs> for FlatTumorNormalMo
                 let af_tumor = AlleleFreq(af_tumor);
                 let p = likelihood_tumor(af_tumor, Some(af_normal)) +
                         likelihood_normal(af_normal, None);
+                debug!("L({})={}", *af_tumor, *p);
                 NotNaN::new(*p).expect("posterior probability is NaN")
             }
         ).into_option().expect("prior has empty allele frequency spectrum");
