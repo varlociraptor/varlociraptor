08621db911f1ab25433e408890df8cad52948f9b
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-05-09 12:28:16 +0200

    Missing files.

---
+++ src/estimation/fdr/mod.rs
@@ -0,0 +1,43 @@
+use bio::stats::PHREDProb;
+
+pub mod bh;
+pub mod mueller;
+
+
+pub const ALPHAS: [f64; 28] = [
+    0.01,
+    0.02,
+    0.03,
+    0.04,
+    0.05,
+    0.06,
+    0.07,
+    0.08,
+    0.09,
+    0.1,
+    0.15,
+    0.2,
+    0.25,
+    0.3,
+    0.35,
+    0.4,
+    0.45,
+    0.5,
+    0.55,
+    0.6,
+    0.65,
+    0.7,
+    0.75,
+    0.8,
+    0.85,
+    0.9,
+    0.95,
+    1.0
+];
+
+
+#[derive(RustcEncodable)]
+struct Record {
+    alpha: f64,
+    gamma: PHREDProb
+}
---
+++ src/estimation/fdr/mueller.rs
@@ -0,0 +1,63 @@
+//! This module implements fully bayesian FDR control as presented by
+//! Müller, Parmigiani, and Rice, "FDR and Bayesian Multiple Comparisons Rules" (July 2006).
+//! Johns Hopkin's University, Dept. of Biostatistics Working Papers. Working Paper 115.
+
+use std::error::Error;
+use std::io;
+
+use bio::stats::{LogProb, PHREDProb, bayesian};
+use itertools::Itertools;
+use rust_htslib::bcf;
+use csv;
+
+use Event;
+use estimation::fdr::{Record, ALPHAS};
+use model;
+use utils;
+
+
+/// Print thresholds to control FDR of given calls at multiple levels.
+///
+/// # Arguments
+///
+/// * `calls` - BCF reader with prosic calls
+/// * `null_calls` - calls under the null model, e.g. obtained by swapping tumor and normal sample
+/// * `writer` - writer for resulting thresholds
+/// * `event` - the event to control
+/// * `vartype` - the variant type to consider
+pub fn control_fdr<E: Event, W: io::Write>(
+    calls: &mut bcf::Reader,
+    writer: &mut W,
+    event: &E,
+    vartype: &model::VariantType) -> Result<(), Box<Error>> {
+    let mut writer = csv::Writer::from_writer(writer).delimiter(b'\t');
+    try!(writer.write(["FDR", "max-prob"].into_iter()));
+
+    let pep_dist = utils::collect_pep_dist(calls, event, vartype)?;
+
+    if pep_dist.is_empty() {
+        for &alpha in &ALPHAS {
+            writer.write([&format!("{}", alpha), ""].iter())?;
+        }
+        return Ok(());
+    }
+
+    // estimate FDR
+    let pep_dist = pep_dist.into_iter().map(|p| LogProb(*p)).collect_vec();
+    let fdrs = bayesian::expected_fdr(&pep_dist);
+
+    for &alpha in ALPHAS.iter().rev() {
+        let ln_alpha = LogProb(alpha.ln());
+        // find the largest pep for which fdr <= alpha
+        // do not let peps with the same value cross the boundary
+        for i in (0..fdrs.len()).rev() {
+            if fdrs[i] <= ln_alpha && (i == 0 || pep_dist[i] != pep_dist[i - 1]) {
+                writer.encode(&Record { alpha: alpha, gamma: PHREDProb::from(pep_dist[i]) })?;
+                break;
+            }
+        }
+    }
+    writer.flush()?;
+
+    Ok(())
+}
