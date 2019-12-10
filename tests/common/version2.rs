e8be1f3b9f1dcbca9df72a21a02530eaba354a0e
Johannes Köster <johannes.koester@uni-due.de>
2019-12-10 10:57:00 +0100

    add missing file

---
+++ tests/common/version2.rs
@@ -0,0 +1,35 @@
+use std::error::Error;
+use std::path::{Path, PathBuf};
+
+use yaml_rust::Yaml;
+
+use crate::common::Testcase;
+
+#[derive(Debug)]
+pub struct TestcaseVersion2 {
+    pub inner: Vec<Yaml>,
+    pub path: PathBuf,
+}
+
+impl Testcase for TestcaseVersion2 {
+    fn inner(&self) -> &[Yaml] {
+        &self.inner
+    }
+
+    fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    fn reference(&self) -> Result<Box<dyn AsRef<Path>>, Box<dyn Error>> {
+        let reference_path = self.path.join(self.reference_path());
+        self.index_reference(&reference_path);
+
+        Ok(Box::new(reference_path.to_owned()))
+    }
+}
+
+impl TestcaseVersion2 {
+    fn reference_path(&self) -> &Path {
+        self.yaml()["reference"]["path"].as_str().unwrap().as_ref()
+    }
+}
