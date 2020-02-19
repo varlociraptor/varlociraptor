acd1a513917d6f442402a8b29119875c93922fa0
David Laehnemann <david.laehnemann@hhu.de>
2020-02-19 14:46:24 +0100

    if insert size estimation finds no fragments, assume single end; switch median to mean (as InsertSize field is named)

--- src/estimation/alignment_properties.rs
+++ src/estimation/alignment_properties.rs
@@ -39,10 +39,11 @@ impl AlignmentProperties {
 
     /// Update maximum observed cigar operation lengths. Return whether any D, I, S, or H operation
     /// was found in the cigar string.
-    pub fn update_max_cigar_ops_len(&mut self, record: &bam::Record) -> bool {
+    pub fn update_max_cigar_ops_len(&mut self, record: &bam::Record) -> (bool, bool) {
         let norm = |j| NotNan::new(j as f64 / record.seq().len() as f64).unwrap();
 
         let mut is_regular = true;
+        let mut has_soft_clip = false;
         for c in record.cigar().iter() {
             match c {
                 &Cigar::SoftClip(l) => {
@@ -50,6 +51,7 @@ impl AlignmentProperties {
                     self.frac_max_softclip =
                         *cmp::max(s, NotNan::new(self.frac_max_softclip).unwrap());
                     is_regular = false;
+                    has_soft_clip = true;
                 }
                 &Cigar::Del(l) => {
                     self.max_del_cigar_len = cmp::max(l, self.max_del_cigar_len);
@@ -66,7 +68,7 @@ impl AlignmentProperties {
             }
         }
 
-        is_regular
+        (is_regular, has_soft_clip)
     }
 
     /// Estimate `AlignmentProperties` from first 10000 fragments of bam file.
@@ -83,25 +85,50 @@ impl AlignmentProperties {
         let mut tlens = Vec::new();
         let mut max_mapq = 0;
         let mut i = 0;
+        let mut skipped = 0;
+        let mut n_soft_clip = 0;
+        let mut n_not_useable = 0;
         while i <= 10000 {
+            if skipped > 100000 {
+                eprint!("\nWARNING: Stopping alignment property estimation after skipping 100000\n\
+                       records and inspecting {} records.\n",i);
+
+                break;
+            }
             if !bam.read(&mut record)? {
                 break;
             }
 
-            if record.is_unmapped() || record.is_duplicate() || record.is_quality_check_failed() {
+            // Records to skip without updating max_cigar_ops_len AND without incrementing the
+            // counter (to keep looking for 10000 useful records for the estimation)
+            if record.mapq() == 0
+                || record.is_duplicate()
+                || record.is_quality_check_failed()
+                || record.is_unmapped() {
+                skipped += 1;
                 continue;
             }
 
             max_mapq = cmp::max(max_mapq, record.mapq());
 
-            let is_regular = properties.update_max_cigar_ops_len(&record);
+            let (is_regular, has_soft_clip) = properties.update_max_cigar_ops_len(&record);
 
-            if is_regular
-                && !record.is_mate_unmapped()
-                && record.is_first_in_template()
-                && record.tid() == record.mtid()
-                && record.mapq() > 0
-            {
+            // Records to skip after updating max_cigar_ops_len, BUT without incrementing the
+            // counter (to keep looking for 10000 useful records for the estimation)
+            if !record.is_paired()
+                || !record.is_first_in_template()
+                || !(record.tid() == record.mtid())
+                || record.is_mate_unmapped() {
+                skipped += 1;
+                continue;
+            }
+
+            if !is_regular {
+                n_not_useable += 1;
+                if has_soft_clip {
+                    n_soft_clip += 1;
+                }
+            } else {
                 // record insert size
                 tlens.push(record.insert_size().abs() as f64);
             }
@@ -109,16 +136,42 @@ impl AlignmentProperties {
             i += 1;
         }
 
-        let upper = tlens.percentile(95);
-        let lower = tlens.percentile(5);
-        let mut valid = tlens
-            .into_iter()
-            .filter(|l| *l <= upper && *l >= lower)
-            .collect_vec();
-        properties.insert_size.mean = valid.median();
-        properties.insert_size.sd = valid.iter().std_dev();
-
-        Ok(properties)
+        if tlens.len() == 0 {
+            eprint!("\nFound no records to use for estimating the insert size. Will assume\n\
+                    single end sequencing data and calculate deletion probabilities without\n\
+                    considering the insert size.\n\
+                    \n\
+                    If your data should be paired end, please consider manually providing\n\
+                    --alignment-properties, e.g. computed with `samtools stats`. Also, maybe\n\
+                    the following counts indicate why no records were usable:\n\n\
+                    - I, D, S or H CIGAR operation: {nu}\n\
+                    - S CIGAR (soft clip, e.g. due to UMIs or adapters): {sc}\n\
+                    \n\
+                    In addition, {nr} records were skipped in the estimation for one\n\
+                    of the following reasons:\n\
+                       - not paired\n\
+                       - not the first segment with regard to the template sequence\n\
+                       - mapping quality of 0\n\
+                       - marked as a duplicate\n\
+                       - mate mapped to different template (e.g. different chromosome)\n\
+                       - failed some quality check according to the 512 SAM flag\n\
+                       - mate unmapped\n\
+                       - unmapped\n",
+                    nu = n_not_useable, sc = n_soft_clip, nr = skipped);
+            properties.insert_size.mean = std::f64::NAN;
+            properties.insert_size.sd = std::f64::NAN;
+            Ok(properties)
+        } else {
+            let upper = tlens.percentile(95);
+            let lower = tlens.percentile(5);
+            let valid = tlens
+                .into_iter()
+                .filter(|l| *l <= upper && *l >= lower)
+                .collect_vec();
+            properties.insert_size.mean = valid.iter().sum::<f64>() / valid.len() as f64;
+            properties.insert_size.sd = valid.iter().std_dev();
+            Ok(properties)
+        }
     }
 
     /// Number of bases that are feasible for overlapping the variant.
@@ -187,11 +240,40 @@ mod tests {
         let props = AlignmentProperties::estimate(&mut bam).unwrap();
         println!("{:?}", props);
 
-        assert_relative_eq!(props.insert_size.mean, 312.0);
-        assert_relative_eq!(props.insert_size.sd, 11.89254089203071);
+        assert_relative_eq!(props.insert_size.mean, 311.9736111111111);
+        assert_relative_eq!(props.insert_size.sd, 11.9001225301502);
         assert_eq!(props.max_del_cigar_len, 30);
         assert_eq!(props.max_ins_cigar_len, 12);
-        assert_eq!(props.frac_max_softclip, 0.69);
+        assert_eq!(props.frac_max_softclip, 0.63);
+    }
+
+    #[test]
+    fn test_estimate_all_reads_have_short_clips() {
+        let mut bam = bam::Reader::from_path("tests/resources/tumor-first30000.reads_with_soft_clips.bam").unwrap();
+
+        let props = AlignmentProperties::estimate(&mut bam).unwrap();
+        println!("{:?}", props);
+
+        assert!(props.insert_size.mean.is_nan());
+        assert!(props.insert_size.sd.is_nan());
+        assert_eq!(props.max_del_cigar_len, 2);
+        assert_eq!(props.max_ins_cigar_len, 4);
+        assert_eq!(props.frac_max_softclip, 0.63);
+    }
+
+    #[test]
+    fn test_estimate_all_reads_single_end() {
+        // this file contains only single-ended reads (artificially made single-ended with awk)
+        let mut bam = bam::Reader::from_path("tests/resources/tumor-first30000.bunch_of_reads_made_single_ended.bam").unwrap();
+
+        let props = AlignmentProperties::estimate(&mut bam).unwrap();
+        println!("{:?}", props);
+
+        assert!(props.insert_size.mean.is_nan());
+        assert!(props.insert_size.sd.is_nan());
+        assert_eq!(props.max_del_cigar_len, 0);
+        assert_eq!(props.max_ins_cigar_len, 0);
+        assert_eq!(props.frac_max_softclip, 0.03);
     }
 
     #[test]
