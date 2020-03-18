891035a445d4dbdc559302d3ed74e44702c72b17
Johannes Köster <johannes.koester@uni-due.de>
2020-03-18 20:45:37 +0100

    playground

--- playground/test-tumor-normal-prior.py
+++ playground/test-tumor-normal-prior.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import seaborn as sns
 import numpy as np
 import math
@@ -18,9 +19,9 @@ def prior_normal(f):
         return HETEROZYGOSITY / m
 
 
-def prior(f_tumor, f_normal):
+def prior(f_tumor, f_normal, effmut=EFFMUT):
     f_somatic = abs(f_tumor - f_normal)
-    p = EFFMUT * (1 / (f_somatic ** 2)) / GENOME_N
+    p = effmut * (1 / (f_somatic ** 2)) / GENOME_N
     return p * prior_normal(f_normal)
     #p = math.log(EFFMUT) - (2 * math.log(f_somatic) + math.log(GENOME_N))
     #return math.exp(p) * prior_normal(f_normal)
@@ -38,3 +39,24 @@ plt.xlabel("tumor allele freq")
 plt.ylabel("density")
 
 plt.savefig("tumor-normal-prior.svg", bbox_inches="tight")
+
+
+fig = plt.figure(figsize=(10, 3), constrained_layout=True)
+spec = gridspec.GridSpec(ncols=3, nrows=1, figure=fig)
+
+for i, f_normal in enumerate([0.0, 0.5, 1.0]):
+    x = np.linspace(0.0, 1.0, 100)
+
+    fig.add_subplot(spec[0, i])
+
+    for effmut in range(1, 1_000_000, 10000):
+        y = [1.0 / prior(f, f_normal, effmut) for f in x]
+    
+        plt.semilogy(x, y, "-", color="grey")
+
+sns.despine()
+plt.legend()
+plt.xlabel("tumor allele freq")
+plt.ylabel("bases until mutation")
+
+plt.savefig("tumor-normal-prior-bases-until.svg", bbox_inches="tight")
