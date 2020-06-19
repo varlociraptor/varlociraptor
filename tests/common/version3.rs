f9c9de4aa0bc4e1a0f6ac727869e046596510c7a
Johannes Köster <johannes.koester@uni-due.de>
2020-06-19 09:41:46 +0200

    add adapter for new testcase version

---
+++ tests/common/version3.rs
@@ -0,0 +1,35 @@
+use std::path::{Path, PathBuf};
+
+use anyhow::Result;
+use yaml_rust::Yaml;
+
+use crate::common::Testcase;
+
+#[derive(Debug)]
+pub struct TestcaseVersion3 {
+    pub inner: Vec<Yaml>,
+    pub path: PathBuf,
+}
+
+impl Testcase for TestcaseVersion3 {
+    fn inner(&self) -> &[Yaml] {
+        &self.inner
+    }
+
+    fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    fn reference(&self) -> Result<Box<dyn AsRef<Path>>> {
+        let reference_path = self.path.join(self.reference_path());
+        self.index_reference(&reference_path);
+
+        Ok(Box::new(reference_path.to_owned()))
+    }
+}
+
+impl TestcaseVersion3 {
+    fn reference_path(&self) -> &Path {
+        self.yaml()["reference"]["path"].as_str().unwrap().as_ref()
+    }
+}
