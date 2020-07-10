1faf2bea55692614f45cffcc41a138532f553eda
Johannes Köster <johannes.koester@uni-due.de>
2020-07-10 07:23:13 +0200

    additional test case

--- tests/lib.rs
+++ tests/lib.rs
@@ -78,6 +78,7 @@ testcase!(test43);
 testcase!(test44);
 testcase!(test45);
 testcase!(test46);
+testcase!(test47);
 testcase!(pattern_too_long);
 testcase!(test_wgbs01);
 testcase!(test_long_pattern);
