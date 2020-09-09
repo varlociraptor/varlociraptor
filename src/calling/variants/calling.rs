6bcd042336c4c40bd3ea9e9c6bf61390c57c2b41
Johannes Köster <johannes.koester@uni-due.de>
2020-09-09 10:57:43 +0200

    do not adjust MAPQ.

--- src/calling/variants/calling.rs
+++ src/calling/variants/calling.rs
@@ -386,7 +386,7 @@ where
             let mut pileup = read_observations(record)?;
             if is_snv_or_mnv {
                 // METHOD: adjust MAPQ to get rid of stochastically inflated ones
-                Observation::adjust_prob_mapping(&mut pileup);
+                //Observation::adjust_prob_mapping(&mut pileup);
                 // METHOD: remove non-standard alignments. They might come from near
                 // SVs and can induce artifactual SNVs or MNVs. By removing them,
                 // we just conservatively reduce the coverage to those which are
