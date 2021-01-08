e75065a5cfe71406c6ad89141a0a42d692ae1c15
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-01-08 10:01:51 +0100

    fmt

--- src/variants/model/bias/read_orientation_bias.rs
+++ src/variants/model/bias/read_orientation_bias.rs
@@ -40,22 +40,21 @@ impl Bias for ReadOrientationBias {
 
     fn is_possible(&self, pileups: &[Vec<Observation<ReadPosition>>]) -> bool {
         pileups.iter().any(|pileup| {
-            pileup
-                .iter()
-                .any(|observation| {
-                    if let ReadOrientationBias::None = *self {
-                        true
-                    } else {
-                        // METHOD: we only statistically consider a bias if there is at least one ALT observation 
-                        // that votes for it without uncertainty. This way, we omit bias estimation in totally 
-                        // uncertain cases, where the bias is actually not observed.
-                        // This behavior is important, because otherwise pathological cases like
-                        // 98Ns**^68Ns***18Vs**^4Vs***2Ns-*^1Np+<*1Np+>*1Ns-**, would weakly vote for a bias,
-                        // although 90% of the reads do not provide orientation info and the only that do are
-                        // reference reads.
-                        self.prob(observation) == LogProb::ln_one() && *observation.bayes_factor_alt() > 1.0
-                    }
-                })
+            pileup.iter().any(|observation| {
+                if let ReadOrientationBias::None = *self {
+                    true
+                } else {
+                    // METHOD: we only statistically consider a bias if there is at least one ALT observation
+                    // that votes for it without uncertainty. This way, we omit bias estimation in totally
+                    // uncertain cases, where the bias is actually not observed.
+                    // This behavior is important, because otherwise pathological cases like
+                    // 98Ns**^68Ns***18Vs**^4Vs***2Ns-*^1Np+<*1Np+>*1Ns-**, would weakly vote for a bias,
+                    // although 90% of the reads do not provide orientation info and the only that do are
+                    // reference reads.
+                    self.prob(observation) == LogProb::ln_one()
+                        && *observation.bayes_factor_alt() > 1.0
+                }
+            })
         })
     }
 }
