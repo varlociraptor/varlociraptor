31a76db0192a569750d54e36836b8a65418eb3ba
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-04-21 20:23:04 +0000

    constrain cache to 1000 items. Properly restrict keys to allele frequencies.

--- src/variants/model/prior.rs
+++ src/variants/model/prior.rs
@@ -10,6 +10,7 @@ use bio::stats::{LogProb, Prob};
 use data_encoding::HEXUPPER;
 use itertools::Itertools;
 use itertools_num::linspace;
+use lru::LruCache;
 use ring::digest;
 use serde_json::{self, json, Value};
 use statrs::distribution::{self, Discrete};
@@ -47,8 +48,21 @@ pub(crate) enum Inheritance {
     },
 }
 
+#[derive(Derefable, Debug)]
+pub struct Cache {
+    #[deref(mutable)]
+    inner: LruCache<Vec<AlleleFreq>, LogProb>,
+}
 
-#[derive(Debug, TypedBuilder, Default, Clone)]
+impl Default for Cache {
+    fn default() -> Self {
+        Cache {
+            inner: LruCache::new(1000),
+        }
+    }
+}
+
+#[derive(Debug, TypedBuilder, Default)]
 pub(crate) struct Prior {
     uniform: grammar::SampleInfo<bool>,
     ploidies: Option<grammar::SampleInfo<Option<u32>>>,
@@ -59,7 +73,23 @@ pub(crate) struct Prior {
     inheritance: grammar::SampleInfo<Option<Inheritance>>,
     genome_size: Option<f64>,
     #[builder(default)]
-    cache: RefCell<BTreeMap<Vec<AlleleFreq>, LogProb>>,
+    cache: RefCell<Cache>,
+}
+
+impl Clone for Prior {
+    fn clone(&self) -> Self {
+        Prior {
+            uniform: self.uniform.clone(),
+            ploidies: self.ploidies.clone(),
+            universe: self.universe.clone(),
+            germline_mutation_rate: self.germline_mutation_rate.clone(),
+            somatic_effective_mutation_rate: self.somatic_effective_mutation_rate.clone(),
+            heterozygosity: self.heterozygosity.clone(),
+            inheritance: self.inheritance.clone(),
+            genome_size: self.genome_size.clone(),
+            cache: RefCell::default(),
+        }
+    }
 }
 
 impl Prior {
@@ -708,14 +738,16 @@ impl bayesian::model::Prior for Prior {
     type Event = Vec<likelihood::Event>;
 
     fn compute(&self, event: &Self::Event) -> LogProb {
-        let key: Vec<_> = event.iter().map(|sample_event| sample_event.allele_freq).collect();
+        let key: Vec<_> = event
+            .iter()
+            .map(|sample_event| sample_event.allele_freq)
+            .collect();
 
-        if let Some(prob) = self.cache.borrow().get(key) {
+        if let Some(prob) = self.cache.borrow_mut().get(&key) {
             return *prob;
         }
-        dbg!(self.cache.len());
         let prob = self.calc_prob(event, Vec::with_capacity(event.len()));
-        self.cache.borrow_mut().insert(key, prob);
+        self.cache.borrow_mut().put(key, prob);
 
         prob
     }
