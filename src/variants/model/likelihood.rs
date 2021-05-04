cd6b86195eefaa42c1dbbb27d7735d4e7cc1f25e
David Laehnemann <david.laehnemann@hhu.de>
2021-05-04 12:38:21 +0200

    add small deletion tumor-normal test case (#155)
    * add small deletion tumor-normal test case provided by @nh13 in issue #154
    
    * actually run the new test case...
    
    * make purity explicitly float
    
    * use reasonable default max cigar indel length for sampling bias estimation
    
    * adapt testcase results
    
    * properly handle missing information about max cigar operation lengths.
    
    * warning
    
    * fmt
    
    Co-authored-by: Johannes Köster <johannes.koester@tu-dortmund.de>

--- src/variants/model/likelihood.rs
+++ src/variants/model/likelihood.rs
@@ -226,6 +226,12 @@ impl Likelihood<SingleSampleCache> for SampleLikelihoodModel {
                 prob + lh
             });
 
+            // dbg!(pileup);
+            // panic!("test");
+            // if *events.secondary.allele_freq == 0.0 {
+            //     dbg!((events, likelihood));
+            // }
+
             assert!(!likelihood.is_nan());
 
             cache.insert(event.clone(), likelihood);
