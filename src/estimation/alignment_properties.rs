10246ee7f464d716ae8414e78fbde7c46d43ce33
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-05-04 12:05:38 +0200

    warning

--- src/estimation/alignment_properties.rs
+++ src/estimation/alignment_properties.rs
@@ -164,11 +164,14 @@ impl AlignmentProperties {
 
         properties.max_read_len = max_read_len;
 
-        if let None = properties.max_del_cigar_len {
+        if properties.max_del_cigar_len.is_none() {
             warn!("No deletion CIGAR operations found in first 10000 alignments. Varlociraptor will be unable to estimate the sampling bias for deletions.");
         }
-        if let None = properties.max_ins_cigar_len {
+        if properties.max_ins_cigar_len.is_none() {
             warn!("No deletion CIGAR operations found in first 10000 alignments. Varlociraptor will be unable to estimate the sampling bias for insertions.");
+        }
+        if properties.frac_max_softclip.is_none() {
+            warn!("No softclip CIGAR operations found in the first 10000 alignments. Varlociraptor will be unable to estimate the sampling bias for larger indels.")
         }
 
         // Mark initial estimation as done.
