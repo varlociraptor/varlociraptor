bf43655b8376c1873476caae1de17144a81cbac9
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-05-12 16:14:48 +0200

    testcase

--- tests/lib.rs
+++ tests/lib.rs
@@ -119,6 +119,7 @@ testcase!(test62, exact);
 testcase!(test63, exact);
 testcase!(test64, exact);
 testcase!(test65, exact);
+testcase!(test69, exact);
 testcase!(test_mendelian_prior, exact);
 testcase!(pattern_too_long, exact, fast);
 testcase!(test_long_pattern, exact, fast);
