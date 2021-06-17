d5a64d272fc890e75270fbfd8d26bf5b5729db3b
huzuner <hamdiyeuzuner@gmail.com>
2021-06-17 10:27:47 +0200

    added cdf method

--- src/calling/haplotype_abundances.rs
+++ src/calling/haplotype_abundances.rs
@@ -1,43 +1,38 @@
-use hdf5;
-use rust_htslib::bcf;
-use derive_builder::Builder;
 use anyhow::Result;
-use std::path::Path;
+use derive_builder::Builder;
+use hdf5;
+use kernel_density;
+use rust_htslib::bcf;
+
 
 #[derive(Builder)]
 #[builder(pattern = "owned")]
-pub(crate) struct Caller<'a> {
-    hdf5_reader: hdf5::Reader<'a>,
+pub(crate) struct Caller {
+    hdf5_reader: hdf5::File,
     vcf_reader: bcf::Reader,
 }
 
-impl<'a> Caller<'a> {
+impl Caller {
+    pub(crate) fn cdf(&self) -> Result<()> {
+        let hdf5 = &self.hdf5_reader;
+        let group = hdf5.group("bootstrap")?;
+        let num_bootstraps = hdf5.dataset("/aux/num_bootstrap")?.read_scalar()?;
+        let mut bootstraps = Vec::new();
+        for i in 0..num_bootstraps {
+            let dataset = group.dataset(&format!("bs{i}", i = i))?;
+            let tpm = dataset.read_1d::<u64>()?;
+            bootstraps.extend(tpm.to_vec());
+        }
+        let n_values = hdf5.dataset("/aux/lengths")?.read_scalar()?;
+        let array = ndarray::Array2::from_shape_vec((num_bootstraps, n_values), bootstraps)?;
+        let array = array.t();
+        let cdf: Vec<_> = array
+            .outer_iter()
+            .map(|x| kernel_density::ecdf::Ecdf::new(&x.to_vec()))
+            .collect();
+        Ok(())
+    }
     pub(crate) fn call(&self) {
         todo!()
     }
 }
-
-
-// impl<'a> CallerBuilder<'a> {
-//     pub(crate) fn new(hdf5_reader: hdf5::Reader, vcf_reader: bcf::Reader) -> CallerBuilder<'a> {
-//         CallerBuilder {
-//             hdf5_reader: hdf5::File::open(hdf5_reader.as_ref()),
-//             vcf_reader: bcf::Reader::from_path(vcf_reader)
-//         }
-//     }
-// }
-
-// impl<'a> Caller<'a> {
-//     pub(crate) fn new<P: AsRef<Path>>(hdf5_path: P, vcf_path: P) -> Result<Self> {
-//         let hdf5_reader = hdf5::File::open(hdf5_path.as_ref())?;
-//         let vcf_reader = bcf::Reader::from_path(vcf_path)?;
-        
-//         Ok (Self {
-//             hdf5_reader,
-//             vcf_reader,
-//         })
-//     }
-//     pub(crate) fn call(&mut self) {
-//         todo!()
-//     }
-//   }
--- src/calling/mod.rs
+++ src/calling/mod.rs
@@ -1,3 +1,3 @@
 //pub(crate) mod cnvs;
-pub(crate) mod variants;
 pub(crate) mod haplotype_abundances;
+pub(crate) mod variants;
