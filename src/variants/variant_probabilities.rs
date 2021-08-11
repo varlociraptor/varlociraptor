c6942717b02dcc4f9e5c915982eb01bbb6e4571d
Felix Mölder <felix.moelder@uni-due.de>
2021-08-11 14:27:53 +0200

    Update plot

--- src/variants/variant_probabilities.rs
+++ src/variants/variant_probabilities.rs
@@ -1,14 +1,15 @@
-use std::str;
-
+use crate::errors;
 use anyhow::Result;
+use bcf::header::HeaderRecord;
 use rust_htslib::bcf::{self, Read};
 use serde_json::{json, Value};
+use std::str;
 
-use crate::errors;
-
+use bio::stats::{PHREDProb, Prob};
 #[derive(Debug, Clone, Serialize)]
 struct ScatterProbs {
     chromosome: String,
+    event: String,
     prob: f32,
     pos: i64,
 }
@@ -18,17 +19,33 @@ pub(crate) fn variant_probabilities() -> Result<()> {
 
     let mut plot_data = Vec::new();
     let header = bcf.header().to_owned();
+    let events: Vec<String> = header
+        .header_records()
+        .into_iter()
+        .filter_map(|x| match x {
+            HeaderRecord::Info { key: _, values } => Some(values["ID"].clone()),
+            _ => None,
+        })
+        .filter(|x| x.starts_with("PROB_"))
+        .collect();
+
     for record in bcf.records() {
         let rec = record.unwrap();
         if let Some(rid) = rec.rid() {
-            let chr = String::from_utf8(header.rid2name(rid)?.to_vec())?;
-            let pos = rec.pos() - rec.rlen();
-            let prob = rec.info(b"PROB_PRESENT").float()?.unwrap()[0];
-            plot_data.push(ScatterProbs {
-                chromosome: chr,
-                prob,
-                pos,
-            });
+            for event in events.iter() {
+                let chr = String::from_utf8(header.rid2name(rid)?.to_vec())?;
+                let pos = rec.pos() - rec.rlen();
+                let phred_prob = rec.info(event.as_bytes()).float()?.unwrap()[0];
+                let prob = *Prob::from(PHREDProb(phred_prob as f64)) as f32;
+                if prob > 0.0 {
+                    plot_data.push(ScatterProbs {
+                        chromosome: chr,
+                        event: event[5..].to_string(),
+                        prob,
+                        pos,
+                    });
+                }
+            }
         }
     }
     if plot_data.is_empty() {
@@ -38,7 +55,7 @@ pub(crate) fn variant_probabilities() -> Result<()> {
     let print_plot = |data: serde_json::Value, blueprint: &str| -> Result<()> {
         let mut blueprint = serde_json::from_str(blueprint)?;
         if let Value::Object(ref mut blueprint) = blueprint {
-            blueprint["data"][0]["values"] = data;
+            blueprint["data"]["values"] = data;
             println!("{}", serde_json::to_string_pretty(blueprint)?);
             Ok(())
         } else {
