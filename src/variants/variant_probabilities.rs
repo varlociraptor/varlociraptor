a454a994cfafc5a9968aa057499c70d326760649
Felix Mölder <felix.moelder@uni-due.de>
2021-08-13 10:12:04 +0200

    Only read specific events

--- src/variants/variant_probabilities.rs
+++ src/variants/variant_probabilities.rs
@@ -1,6 +1,5 @@
 use crate::errors;
 use anyhow::Result;
-use bcf::header::HeaderRecord;
 use rust_htslib::bcf::{self, Read};
 use serde_json::{json, Value};
 use std::str;
@@ -19,32 +18,35 @@ pub(crate) fn variant_probabilities() -> Result<()> {
 
     let mut plot_data = Vec::new();
     let header = bcf.header().to_owned();
-    let events: Vec<String> = header
-        .header_records()
-        .into_iter()
-        .filter_map(|x| match x {
-            HeaderRecord::Info { key: _, values } => Some(values["ID"].clone()),
-            _ => None,
-        })
-        .filter(|x| x.starts_with("PROB_"))
-        .collect();
-
     for record in bcf.records() {
         let rec = record.unwrap();
         if let Some(rid) = rec.rid() {
-            for event in events.iter() {
-                let chr = String::from_utf8(header.rid2name(rid)?.to_vec())?;
-                let pos = rec.pos() - rec.rlen();
-                let phred_prob = rec.info(event.as_bytes()).float()?.unwrap()[0];
-                let prob = *Prob::from(PHREDProb(phred_prob as f64)) as f32;
-                if prob > 0.0 {
-                    plot_data.push(ScatterProbs {
-                        chromosome: chr,
-                        event: event[5..].to_string(),
-                        prob,
-                        pos,
-                    });
+            let chr = String::from_utf8(header.rid2name(rid)?.to_vec())?;
+            let pos = rec.pos() - rec.rlen();
+            let max_prob = |(event_a, prob_a), (event_b, prob_b)| {
+                if prob_a < prob_b {
+                    (event_a, *Prob::from(PHREDProb(prob_a as f64)) as f32)
+                } else {
+                    (event_b, *Prob::from(PHREDProb(prob_b as f64)) as f32)
                 }
+            };
+            let (event, prob) = max_prob(
+                (
+                    String::from("PROB_PRESENT"),
+                    rec.info(b"PROB_PRESENT").float().unwrap().unwrap()[0],
+                ),
+                (
+                    String::from("PROB_FFPE_ARTIFACT"),
+                    rec.info(b"PROB_FFPE_ARTIFACT").float().unwrap().unwrap()[0],
+                ),
+            );
+            if prob > 0.0 {
+                plot_data.push(ScatterProbs {
+                    chromosome: chr,
+                    event,
+                    prob,
+                    pos,
+                });
             }
         }
     }
