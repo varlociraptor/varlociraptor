c85110feb4a68afef846012a2690f349ea2a46f6
Till Hartmann <till.hartmann@udo.edu>
2021-08-23 23:13:00 +0200

    do not store vafs, only lfc

--- src/utils/log2_fold_change.rs
+++ src/utils/log2_fold_change.rs
@@ -3,22 +3,21 @@ use std::ops::Not;
 use ordered_float::NotNan;
 
 use crate::utils::comparison::ComparisonOperator;
-use crate::variants::model::AlleleFreq;
 use std::fmt::{Display, Formatter};
 
-#[derive(Debug, Clone)]
-pub(crate) struct Log2FoldChange {
-    vaf_a: AlleleFreq,
-    vaf_b: AlleleFreq,
-    value: f64,
-}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Derefable)]
+pub(crate) struct Log2FoldChange(#[deref] NotNan<f64>);
 
 impl Log2FoldChange {
-    pub(crate) fn new(vaf_a: AlleleFreq, vaf_b: AlleleFreq) -> Self {
-        Self {
-            vaf_a,
-            vaf_b,
-            value: vaf_a.log2() - vaf_b.log2(),
+    /// Calculate log2 fold change of a by b (i.e. (a / b).log2()).
+    /// For a == b == 0.0, returns 0.0.
+    /// Returns None if fold change cannot be calculated (if both a and b are zero).
+    pub(crate) fn new(a: NotNan<f64>, b: NotNan<f64>) -> Self {
+        if *a == 0.0 && *b == 0.0 {
+            Log2FoldChange(NotNan::new(0.0).unwrap())
+        } else {
+            let value = a.log2() - b.log2();
+            Log2FoldChange(NotNan::new(value).expect("bug: NaN when calculating Log2FoldChange"))
         }
     }
 }
@@ -39,13 +38,14 @@ impl Display for Log2FoldChangePredicate {
 impl Log2FoldChangePredicate {
     pub(crate) fn is_true(&self, lfc: &Log2FoldChange) -> bool {
         let v = *self.value;
+        let lfc = *lfc.0;
         match self.comparison {
-            ComparisonOperator::Equal => relative_eq!(lfc.value, v),
-            ComparisonOperator::Greater => lfc.value > v,
-            ComparisonOperator::GreaterEqual => lfc.value >= v,
-            ComparisonOperator::Less => lfc.value < v,
-            ComparisonOperator::LessEqual => lfc.value <= v,
-            ComparisonOperator::NotEqual => relative_ne!(lfc.value, v),
+            ComparisonOperator::Equal => relative_eq!(lfc, v),
+            ComparisonOperator::Greater => lfc > v,
+            ComparisonOperator::GreaterEqual => lfc >= v,
+            ComparisonOperator::Less => lfc < v,
+            ComparisonOperator::LessEqual => lfc <= v,
+            ComparisonOperator::NotEqual => relative_ne!(lfc, v),
         }
     }
 }
