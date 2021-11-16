006fcdf5828650f7f735d39fae3858195350090b
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-11-16 15:10:11 +0100

    fixes

--- src/utils/mapq_adjustment.rs
+++ src/utils/mapq_adjustment.rs
@@ -6,7 +6,7 @@ use rgsl::randist::multinomial::multinomial_lnpdf;
 use crate::variants::evidence::observation::{Observation, ReadPosition};
 
 pub(crate) type Mapq = u32;
-pub(crate) type ObservedMapqs = Counter<Mapq>;
+pub(crate) type ObservedMapqs = Vec<Mapq>;
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
 pub(crate) enum LocusType {
@@ -45,9 +45,24 @@ impl bayesian::model::Posterior for Posterior {
     }
 }
 
-#[derive(Clone, Debug, new)]
+#[derive(Clone, Debug)]
 pub(crate) struct Likelihood {
-    max_mapq: Mapq,
+    p_unique: Vec<f64>,
+    p_homologous: Vec<f64>,
+}
+
+impl Likelihood {
+    pub(crate) fn new(max_mapq: Mapq) -> Self {
+        let mut p_unique = (0..max_mapq).into_iter().map(|mapq| 0.0).collect_vec();
+        p_unique.push(1.0);
+        Likelihood {
+            p_unique,
+            p_homologous: (0..=max_mapq)
+                .into_iter()
+                .map(|mapq| 1.0 / max_mapq as f64)
+                .collect_vec(),
+        }
+    }
 }
 
 impl bayesian::model::Likelihood for Likelihood {
@@ -56,23 +71,12 @@ impl bayesian::model::Likelihood for Likelihood {
     type Data = ObservedMapqs;
 
     fn compute(&self, event: &Self::Event, data: &Self::Data, _payload: &mut ()) -> LogProb {
-        let obs = (0..self.max_mapq)
-            .into_iter()
-            .map(|mapq| data.get(&mapq).cloned().unwrap_or(0) as u32)
-            .collect_vec();
-        match event {
-            LocusType::Unique => LogProb(multinomial_lnpdf(
-                &(0..self.max_mapq).into_iter().map(|mapq| 0.0).collect_vec(),
-                &obs,
-            )),
-            LocusType::Homologous => LogProb(multinomial_lnpdf(
-                &(0..self.max_mapq + 1)
-                    .into_iter()
-                    .map(|mapq| 1.0 / self.max_mapq as f64)
-                    .collect_vec(),
-                &obs,
-            )),
-        }
+        let p = match event {
+            LocusType::Unique => LogProb(multinomial_lnpdf(&self.p_unique, data)),
+            LocusType::Homologous => LogProb(multinomial_lnpdf(&self.p_homologous, data)),
+        };
+        dbg!((data, p, &self.p_unique, &self.p_homologous));
+        p
     }
 }
 
@@ -84,11 +88,16 @@ pub(crate) fn adjust_prob_mapping(pileup: &mut [Observation<ReadPosition>]) {
         // The rationale for the former case is that we always expect some noise of artificially high MAPQs because alleles can be unknown by the mapper
         // and sometimes base errors make a read randomly fit better than it should.
 
-        let data: Counter<_> = pileup
+        let mapqs: Counter<_> = pileup
             .iter()
-            .map(|obs| PHREDProb::from(obs.prob_mapping_orig()).round() as u32)
+            .map(|obs| {PHREDProb::from(obs.prob_mapping_orig().ln_one_minus_exp()).round() as u32})
             .collect();
-        let max_mapq = *data.keys().max().unwrap();
+        let max_mapq = *mapqs.keys().max().unwrap();
+        dbg!(max_mapq);
+        let data = (0..=max_mapq)
+            .into_iter()
+            .map(|mapq| mapqs.get(&mapq).cloned().unwrap_or(0) as u32)
+            .collect_vec();
 
         let model = bayesian::model::Model::new(Likelihood::new(max_mapq), Prior, Posterior);
 
@@ -97,6 +106,7 @@ pub(crate) fn adjust_prob_mapping(pileup: &mut [Observation<ReadPosition>]) {
         // Calculate expected value of MAPQ given the two posteriors and their repective MAPQs (0 and max_mapq).
         let adjusted =
             m.posterior(&LocusType::Unique).unwrap() + LogProb::from(PHREDProb(max_mapq as f64));
+        dbg!(adjusted);
         for obs in pileup {
             if adjusted < obs.prob_mapping_orig() {
                 obs.set_prob_mapping_adj(adjusted);
