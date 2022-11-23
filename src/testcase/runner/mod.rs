4a2e12295a288e48b427693bf094b7733bc6552f
Johannes Köster <johannes.koester@tu-dortmund.de>
2022-11-23 13:13:36 +0100

    feat: expose testcase API in library (#308)
    ### Description
    
    <!--Add a description of your PR here-->
    
    ### QC
    <!-- Make sure that you can tick the boxes below. -->
    
    * [x] The PR contains a test case for the changes or the changes are
    already covered by an existing test case.
    * [x] The documentation at
    https://github.com/varlociraptor/varlociraptor.github.io is updated in a
    separate PR to reflect the changes or this is not necessary (e.g. if the
    change does neither modify the calling grammar nor the behavior or
    functionalities of Varlociraptor).

---
+++ src/testcase/runner/mod.rs
@@ -0,0 +1,79 @@
+pub mod common;
+
+#[macro_export]
+macro_rules! testcase {
+    ($name:ident, $($pairhmm_mode:ident),+) => {
+        paste! {
+            lazy_static! {
+                static ref [<$name:upper _MUTEX>]: Mutex<()> = Mutex::new(());
+            }
+
+            $(
+                #[test]
+                fn [<$name _ $pairhmm_mode _mode>]() {
+                    use crate::testcase::runner::common::load_testcase;
+                    // Poison error can be ignored here, because it just means that the other test failed
+                    // and we are safe to go on.
+                    let _guard = [<$name:upper _MUTEX>].lock();
+                    let name = stringify!($name);
+                    let testcase = load_testcase(
+                        &Path::new(file!())
+                            .parent()
+                            .unwrap()
+                            .join("resources/testcases")
+                            .join(name),
+                    )
+                    .unwrap();
+                    let mode = stringify!($pairhmm_mode);
+
+                    // setup logger
+                    // fern::Dispatch::new()
+                    // .level(log::LevelFilter::Info)
+                    // .chain(std::io::stderr())
+                    // .apply()
+                    // .unwrap();
+
+                    testcase.run(mode).unwrap();
+                    testcase.check();
+                }
+            )*
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! testcase_should_panic {
+    ($name:ident, $($pairhmm_mode:ident),+) => {
+        paste! {
+            lazy_static! {
+                static ref [<$name:upper _MUTEX>]: Mutex<()> = Mutex::new(());
+            }
+
+            $(
+                #[should_panic]
+                #[test]
+                fn [<$name _ $pairhmm_mode _mode>]() {
+                    use crate::testcase::runner::common::load_testcase;
+                    // Poison error can be ignored here, because it just means that the other test failed
+                    // and we are safe to go on.
+                    let _guard = [<$name:upper _MUTEX>].lock();
+                    let name = stringify!($name);
+                    let testcase = load_testcase(
+                        &Path::new(file!())
+                            .parent()
+                            .unwrap()
+                            .join("resources/testcases")
+                            .join(name),
+                    )
+                    .unwrap();
+                    let mode = stringify!($pairhmm_mode);
+                    testcase.run(mode).unwrap();
+                    testcase.check();
+                }
+            )*
+        }
+    };
+}
+
+pub use testcase;
+pub use testcase_should_panic;
