d3b881cac51ac0555a446307af2bf5c881a5d160
Johannes Köster <johannes.koester@tu-dortmund.de>
2022-12-15 19:29:27 +0100

    feat: add MATEID based recognition of connected breakends (#313)
    Before, only EVENT tags where supported. Now, also MATEID without an
    EVENT tag is enough to construct a pair of connected breakends.
    
    ### QC
    <!-- Make sure that you can tick the boxes below. -->
    
    * [x] The PR contains a test case for the changes or the changes are
    already covered by an existing test case.
    * [x] The documentation at
    https://github.com/varlociraptor/varlociraptor.github.io is updated in a
    separate PR to reflect the changes or this is not necessary (e.g. if the
    change does neither modify the calling grammar nor the behavior or
    functionalities of Varlociraptor).

--- src/variants/model/mod.rs
+++ src/variants/model/mod.rs
@@ -6,13 +6,14 @@
 use std::cmp::Ordering;
 use std::fmt::Debug;
 use std::ops::{Deref, Range};
-use std::str;
+use std::{mem, str};
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use ordered_float::NotNan;
 use rust_htslib::bcf;
 use strum_macros::{EnumIter, EnumString, IntoStaticStr};
 
+use crate::errors::Error;
 use crate::grammar;
 use crate::variants::model::bias::Artifacts;
 
@@ -153,6 +154,19 @@ impl HaplotypeIdentifier {
         if let Ok(Some(event)) = record.info(b"EVENT").string() {
             let event = event[0];
             return Ok(Some(HaplotypeIdentifier::Event(event.to_owned())));
+        }
+
+        if let Ok(Some(mateid)) = record.info(b"MATEID").string() {
+            let recid = record.id();
+            if recid != b"." {
+                let mateid = mateid[0].to_owned();
+                let mut ids = [recid.as_slice(), mateid.as_slice()];
+                ids.sort();
+                let event: Vec<u8> = ids.join(b"-".as_slice()).into();
+                return Ok(Some(HaplotypeIdentifier::Event(event)));
+            } else {
+                bail!(Error::BreakendMateidWithoutRecid);
+            }
         }
 
         // TODO support phase sets in the future
