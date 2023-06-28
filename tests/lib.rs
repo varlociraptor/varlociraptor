b03fc0001c1ee237a4ef5c92c460701fdf83d6db
Johannes Koester <johannes.koester@uk-essen.de>
2023-06-28 16:01:10 +0000

    rename and comment in test case

--- tests/lib.rs
+++ tests/lib.rs
@@ -180,6 +180,7 @@ testcase!(test_imprecise_fusion_absent, exact);
 testcase!(test_uzuner_clonal_1, exact);
 testcase!(test_uzuner_clonal_2, exact);
 testcase!(test_uzuner_clonal_3, exact);
+testcase!(test_uzuner_fp_snv_with_realignment, exact);
 
 fn basedir(test: &str) -> String {
     format!("tests/resources/{}", test)
