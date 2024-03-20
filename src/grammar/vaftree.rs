7d1c0573f583dbfc01a60d8eeef68e6b3ea24c7e
fxwiegand <fxwiegand@wgdnet.de>
2024-03-20 13:27:29 +0100

    Lints

--- src/grammar/vaftree.rs
+++ src/grammar/vaftree.rs
@@ -39,7 +39,7 @@ impl VAFTree {
         }
     }
 
-    pub fn contains(&self, operands: &LikelihoodOperands, exclude_sample: Option<usize>) -> bool {
+    pub(crate) fn contains(&self, operands: &LikelihoodOperands, exclude_sample: Option<usize>) -> bool {
         self.inner.iter().any(|node| {
             let mut lfcs = operands.lfcs().iter().collect();
             node.contains(operands, &mut lfcs, exclude_sample)
@@ -161,7 +161,7 @@ impl Node {
 }
 
 impl VAFTree {
-    pub fn new(formula: &NormalizedFormula, scenario: &Scenario, contig: &str) -> Result<Self> {
+    pub(crate) fn new(formula: &NormalizedFormula, scenario: &Scenario, contig: &str) -> Result<Self> {
         fn from(formula: &NormalizedFormula, scenario: &Scenario) -> Result<Vec<Node>> {
             match formula {
                 NormalizedFormula::Atom { sample, vafs } => {
