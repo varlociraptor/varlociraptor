08139ecbfb4afcee93c1bda5d66552e40ba701c1
Addimator <adpri100@hhu.de>
2025-05-05 14:25:01 +0200

    tidy up

--- src/candidates/cnv.rs
+++ src/candidates/cnv.rs
@@ -1,6 +1,7 @@
 use anyhow::{Context, Result};
 use bio_types::genome::{AbstractLocus, Locus};
 use rust_htslib::bcf::header::HeaderView;
+use rust_htslib::bcf::record::Numeric;
 use rust_htslib::bcf::{Format, Header, Read, Reader, Record, Writer};
 use std::path::PathBuf;
 
@@ -8,11 +9,27 @@ use crate::utils::aux_info::AuxInfoCollector;
 use crate::variants::model::VariantPrecision;
 use crate::variants::types::breakends::Breakend;
 
-/// GRIDSS calculates quality scores according to the model outlined in the paper.
-/// As GRIDSS does not yet perform multiple test correction or score recalibration, QUAL scores are vastly overestimated for all variants.
-/// As a rule of thumb, variants that have QUAL >= 1000 and have assemblies from both sides of the breakpoint (AS > 0 & RAS > 0) are considered of high quality,
-/// variants with QUAL >= 500 but that can only be assembled from one breakend (AS > 0 | RAS > 0) are considered of intermediate quality,
-/// and variants with low QUAL score or lack any supporting assemblies are considered to be of low quality.
+/// Builds a new BCF header based on the input header
+fn create_header_from_existing(existing: &HeaderView) -> Result<Header> {
+    let mut header = Header::new();
+
+    for rid in 0..existing.contig_count() {
+        let name =
+            std::str::from_utf8(existing.rid2name(rid)?).context("Invalid UTF-8 in contig name")?;
+        let header_contig_line = format!(r#"##contig=<ID={}>"#, name);
+        header.push_record(header_contig_line.as_bytes());
+    }
+
+    header.push_record(
+        b"##INFO=<ID=QUAL,Number=1,Type=String,Description=\"Variant classification\">",
+    );
+    header.push_record(
+        b"##INFO=<ID=ENDING,Number=1,Type=String,Description=\"Ending position of breakend\">",
+    );
+
+    Ok(header)
+}
+
 fn create_breakend_from_record(
     record: &Record,
     header: &HeaderView,
@@ -60,9 +77,14 @@ fn create_record(record: &Record, bcf_writer: &Writer, breakend: Breakend) -> Re
     let old_allele = record.alleles()[0].to_vec();
     cnv_record.set_alleles(&[old_allele.as_slice(), b"<CNV>"])?;
 
+    // GRIDSS calculates quality scores according to the model outlined in the paper.
+    // As GRIDSS does not yet perform multiple test correction or score recalibration, QUAL scores are vastly overestimated for all variants.
+    // As a rule of thumb, variants that have QUAL >= 1000 and have assemblies from both sides of the breakpoint (AS > 0 & RAS > 0) are considered of high quality,
+    // variants with QUAL >= 500 but that can only be assembled from one breakend (AS > 0 | RAS > 0) are considered of intermediate quality,
+    // and variants with low QUAL score or lack any supporting assemblies are considered to be of low quality.
     let as_val = get_first_info_int(record, b"AS");
     let ras_val = get_first_info_int(record, b"RAS");
-
+    cnv_record.set_qual(f32::missing());
     let gridss_qual = match (as_val, ras_val) {
         (Some(1), Some(1)) if record.qual() > 1000.0 => "HIGH",
         (Some(1), _) | (_, Some(1)) if record.qual() > 500.0 => "MEDIUM",
@@ -71,35 +93,16 @@ fn create_record(record: &Record, bcf_writer: &Writer, breakend: Breakend) -> Re
     cnv_record
         .push_info_string(b"QUAL", &[gridss_qual.as_bytes()])
         .with_context(|| "Failed to push QUAL info string")?;
-    let mate_locus = breakend.join().as_ref().unwrap().locus();
+
+    let join = breakend.join().as_ref().unwrap();
+    let mate_locus = join.locus();
     let end_value = format!("{}:{}", mate_locus.contig(), mate_locus.pos());
     let end = end_value.as_str();
     cnv_record
-        .push_info_string(b"END", &[end.as_bytes()])
+        .push_info_string(b"ENDING", &[end.as_bytes()])
         .with_context(|| "Failed to push END info string")?;
 
     Ok(cnv_record)
-}
-
-/// Builds a new BCF header based on the input header
-fn create_header_from_existing(existing: &HeaderView) -> Result<Header> {
-    let mut header = Header::new();
-
-    for rid in 0..existing.contig_count() {
-        let name =
-            std::str::from_utf8(existing.rid2name(rid)?).context("Invalid UTF-8 in contig name")?;
-        let header_contig_line = format!(r#"##contig=<ID={}>"#, name);
-        header.push_record(header_contig_line.as_bytes());
-    }
-
-    header.push_record(
-        b"##INFO=<ID=QUAL,Number=1,Type=String,Description=\"Variant classification\">",
-    );
-    header.push_record(
-        b"##INFO=<ID=END,Number=1,Type=String,Description=\"Ending position of breakend\">",
-    );
-
-    Ok(header)
 }
 
 /// Reads an input BCF file, processes each record, and writes out modified CNV records
