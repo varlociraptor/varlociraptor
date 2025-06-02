af8bb02edbf351cd603507b680142cc4c0993796
Addimator <adpri100@hhu.de>
2025-06-02 21:42:59 +0200

    Change whole implementation to really find CNV intervals and not just copy breakends

--- src/candidates/cnv.rs
+++ src/candidates/cnv.rs
@@ -1,13 +1,62 @@
 use anyhow::{Context, Result};
 use bio_types::genome::{AbstractLocus, Locus};
 use rust_htslib::bcf::header::HeaderView;
-use rust_htslib::bcf::record::Numeric;
 use rust_htslib::bcf::{Format, Header, Read, Reader, Record, Writer};
+use std::collections::BTreeMap;
 use std::path::PathBuf;
 
 use crate::utils::aux_info::AuxInfoCollector;
 use crate::variants::model::VariantPrecision;
 use crate::variants::types::breakends::Breakend;
+
+#[derive(Debug)]
+pub struct Interval {
+    pub start: Locus,
+    pub end: Locus,
+    pub dir: i32,
+}
+
+impl Interval {
+    pub fn new(start: Locus, end: Locus, dir: i32) -> Self {
+        Self { start, end, dir }
+    }
+}
+
+fn write_records(intervals: Vec<Interval>, header: &Header, outbcf: Option<PathBuf>) -> Result<()> {
+    let mut bcf_writer = match outbcf {
+        Some(path) => Writer::from_path(path, header, true, Format::Bcf)
+            .with_context(|| "Error opening BCF writer".to_string())?,
+        None => Writer::from_stdout(header, true, Format::Bcf)
+            .with_context(|| "Error opening BCF writer".to_string())?,
+    };
+
+    for interval in intervals {
+        let mut cnv_type = b"NAN";
+        if interval.dir < 0 {
+            cnv_type = b"INS";
+        }
+        if interval.dir > 0 {
+            cnv_type = b"DEL";
+        }
+        let mut cnv_record = bcf_writer.empty_record();
+        cnv_record.set_pos(interval.start.pos() as i64);
+        cnv_record.set_qual(f32::NAN);
+        cnv_record.set_alleles(&[b"<CNV>"])?;
+        // TODO: Get the chromosome dynamically
+        let end_value = format!("{}:{}", interval.end.contig(), interval.end.pos());
+        let end = end_value.as_str();
+        cnv_record
+            .push_info_string(b"ENDING", &[end.as_bytes()])
+            .with_context(|| "Failed to push END info string")?;
+
+        cnv_record.push_info_string(b"CNVTYPE", &[cnv_type])?;
+
+        bcf_writer
+            .write(&cnv_record)
+            .with_context(|| "Failed to write BCF record".to_string())?;
+    }
+    Ok(())
+}
 
 /// Builds a new BCF header based on the input header
 fn create_header_from_existing(existing: &HeaderView) -> Result<Header> {
@@ -20,13 +69,10 @@ fn create_header_from_existing(existing: &HeaderView) -> Result<Header> {
         header.push_record(header_contig_line.as_bytes());
     }
 
-    header.push_record(
-        b"##INFO=<ID=QUAL_CLASS,Number=1,Type=String,Description=\"Variant classification\">",
-    );
+    header.push_record(b"##INFO=<ID=CNVTYPE,Number=1,Type=String,Description=\"Type of CNV\">");
     header.push_record(
         b"##INFO=<ID=ENDING,Number=1,Type=String,Description=\"Ending position of breakend\">",
     );
-
     Ok(header)
 }
 
@@ -55,57 +101,33 @@ fn create_breakend_from_record(
     breakend
 }
 
-/// Helper function to extract the first integer value from an INFO field
-fn get_first_info_int(record: &Record, tag: &[u8]) -> Option<i32> {
-    record
-        .info(tag)
-        .integer()
-        .ok()
-        .flatten()
-        .and_then(|v| v.first().copied())
-}
+fn breakends_to_intervals(breakends: Vec<Breakend>) -> Vec<Interval> {
+    // For every Cnv interval set the start of the interval to +1 and the end of the interval to -1
+    let mut deltas = BTreeMap::new();
+    for b in breakends {
+        if let Some(join) = b.join() {
+            let start = b.locus().to_owned();
+            let end = join.locus().to_owned();
+            dbg!(&start, &end);
+            *deltas.entry(start).or_insert(0) += 1;
+            *deltas.entry(end).or_insert(0) -= 1;
+        }
+    }
+    // Compute intervals from borders
+    let mut intervals_deltas = Vec::new();
+    let mut last_pos: Option<Locus> = None;
+    let mut current_cov = 0;
 
-/// Creates a new CNV record based on an existing BCF record
-fn create_record(record: &Record, bcf_writer: &Writer, breakend: Breakend) -> Result<Record> {
-    let mut cnv_record = bcf_writer.empty_record();
-
-    cnv_record.set_rid(record.rid());
-    cnv_record.set_pos(record.pos());
-    cnv_record.set_qual(f32::NAN); // Sets quality to missing
-
-    let old_allele = record.alleles()[0].to_vec();
-    cnv_record.set_alleles(&[old_allele.as_slice(), b"<CNV>"])?;
-
-    // GRIDSS calculates quality scores according to the model outlined in the paper.
-    // As GRIDSS does not yet perform multiple test correction or score recalibration, QUAL scores are vastly overestimated for all variants.
-    // As a rule of thumb, variants that have QUAL >= 1000 and have assemblies from both sides of the breakpoint (AS > 0 & RAS > 0) are considered of high quality,
-    // variants with QUAL >= 500 but that can only be assembled from one breakend (AS > 0 | RAS > 0) are considered of intermediate quality,
-    // and variants with low QUAL score or lack any supporting assemblies are considered to be of low quality.
-    let as_val = get_first_info_int(record, b"AS");
-    let ras_val = get_first_info_int(record, b"RAS");
-    let asm_left = as_val.unwrap_or(0) > 0;
-    let asm_right = ras_val.unwrap_or(0) > 0;
-    let gridss_qual = match (asm_left, asm_right, record.qual()) {
-        (true, true, q) if q >= 900.0 => "HIGH",
-        (true, false, q) | (false, true, q) if q >= 500.0 => "MEDIUM",
-        _ => "LOW",
-    };
-    cnv_record
-        .push_info_string(b"QUAL_CLASS", &[gridss_qual.as_bytes()])
-        .with_context(|| "Failed to push QUAL_CLASS info string")?;
-
-    let join = breakend
-        .join()
-        .as_ref()
-        .with_context(|| "Failed to get join from breakend")?;
-    let mate_locus = join.locus();
-    let end_value = format!("{}:{}", mate_locus.contig(), mate_locus.pos());
-    let end = end_value.as_str();
-    cnv_record
-        .push_info_string(b"ENDING", &[end.as_bytes()])
-        .with_context(|| "Failed to push END info string")?;
-
-    Ok(cnv_record)
+    for (locus, &delta) in deltas.iter() {
+        if let Some(prev_locus) = last_pos {
+            if prev_locus.pos() < locus.pos() && current_cov != 0 {
+                intervals_deltas.push(Interval::new(prev_locus, locus.to_owned(), current_cov));
+            }
+        }
+        current_cov += delta;
+        last_pos = Some(locus.to_owned());
+    }
+    intervals_deltas
 }
 
 /// Reads an input BCF file, processes each record, and writes out modified CNV records
@@ -115,28 +137,23 @@ pub fn find_candidates(breakends_bcf: PathBuf, outbcf: Option<PathBuf>) -> Resul
 
     let header = create_header_from_existing(bcf_reader.header())?;
 
-    let mut bcf_writer = match outbcf {
-        Some(path) => Writer::from_path(path, &header, true, Format::Bcf)
-            .with_context(|| "Error opening BCF writer".to_string())?,
-        None => Writer::from_stdout(&header, true, Format::Bcf)
-            .with_context(|| "Error opening BCF writer".to_string())?,
-    };
+    let mut breakends = Vec::new();
 
     let header_orig = bcf_reader.header().clone();
     let aux_info = AuxInfoCollector::new(&[], &bcf_reader)
         .with_context(|| "Failed to create AuxInfoCollector")?;
+    // Get all breakends from the input BCF file
     for record_result in bcf_reader.records() {
         let record = record_result.with_context(|| "Error reading record")?;
         let breakend = match create_breakend_from_record(&record, &header_orig, &aux_info) {
             Some(b) if b.is_left_to_right() && b.join().is_some() => b,
-            _ => continue, // skip single-sided or right-to-left bnds
+            _ => continue,
         };
-
-        let cnv_record = create_record(&record, &bcf_writer, breakend)?;
-
-        bcf_writer
-            .write(&cnv_record)
-            .with_context(|| "Failed to write BCF record".to_string())?;
+        breakends.push(breakend);
     }
+    let intervals: Vec<Interval> = breakends_to_intervals(breakends);
+    // Write bcf file
+    write_records(intervals, &header, outbcf)?;
+
     Ok(())
 }
