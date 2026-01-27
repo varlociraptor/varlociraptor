6678c1df66718d4bd927bfd020061e4eb79d7731
Addimator <adpri100@hhu.de>
2026-01-28 00:45:13 +0100

    Add testcases

--- tests/lib.rs
+++ tests/lib.rs
@@ -183,7 +183,8 @@ testcase!(test_prinz_af_scan, exact);
 testcase!(test_prinz_call_meth_1, exact);
 testcase!(test_prinz_call_meth_2, exact);
 testcase!(test_prinz_pacbio_zero, exact);
-testcase!(test_prinz_ceta_one_sample, exact);
+testcase!(test_prinz_ceta_one_sample_emseq, exact);
+testcase!(test_prinz_ceta_one_sample_untreated, exact);
 testcase!(test_prinz_ceta_two_samples, exact);
 
 testcase!(test_imprecise_fusion, exact);
